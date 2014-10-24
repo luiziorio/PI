27d2d8d4402bb63a497fc2666689ea45ace23a11
lillo42 <andrade_177@hotmail.com>
2014-10-24 18:55:22 -0200

    Add util

---
+++ Source_Code/FlightSystemCompomente/src/Criptografia/Cripto.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Criptografia;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rafael
+ */
+public class Cripto
+{
+    /**
+     * Encripta um texto usando o algoritimo  de hash MD5
+     * @param texto Texto a sem encriptogrado
+     * @return retorna o texto encriptografado
+     */
+    public static String encripta(String texto)
+    {
+        String retonro = "";
+        MessageDigest algorithm;
+        try
+        {
+            algorithm = MessageDigest.getInstance("MD5");
+            byte messageDigest[] = algorithm.digest(texto.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) 
+                hexString.append(String.format("%02X", 0xFF & b));
+            retonro = hexString.toString();
+        }
+        catch (NoSuchAlgorithmException ex)
+        {
+            
+        }
+        catch(UnsupportedEncodingException ex)
+        {
+            
+        }
+        return retonro;
+    }
+}
