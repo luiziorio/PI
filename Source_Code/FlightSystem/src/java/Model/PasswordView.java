d84e8ca5263cc6241c5605aafe0c88b072454507
FlavinhaIn <flaviadeamm@gmail.com>
2014-11-06 14:30:57 -0200

    Alterações de layout
    Alterações na Home e Login

---
+++ Source_Code/FlightSystem/src/java/Model/PasswordView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Flavinha
+ */
+public class PasswordView {
+     
+    private String password5;
+   
+    public String getPassword5() {
+        return password5;
+    }
+ 
+    public void setPassword5(String password5) {
+        this.password5 = password5;
+    }  
+}
+
