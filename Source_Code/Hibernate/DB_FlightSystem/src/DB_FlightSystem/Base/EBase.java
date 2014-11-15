455c60bf417c8a0417b95595daef14e59126499b
FlavinhaIn <flaviadeamm@gmail.com>
2014-11-15 15:39:50 -0200

    Hibernate
    Hiebernate ajuste para banco sql

--- Source_Code/Hibernate/DB_FlightSystem/src/DB_FlightSystem/Base/EBase.java
+++ Source_Code/Hibernate/DB_FlightSystem/src/DB_FlightSystem/Base/EBase.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
@@ -24,7 +25,7 @@ import javax.persistence.InheritanceType;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class EBase implements Serializable
 {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.TABLE)
     protected int sequencial;
 
 
